859d757c7ec1fa5d1fccd2be542734f21bfaabcf
IlikeTrains <sewerynpresnal@gmail.com>
2021-01-04 21:41:02 +0100

    Knight moves

--- ChessPP/knight.cpp
+++ ChessPP/knight.cpp
@@ -7,3 +7,86 @@ const std::string Knight::getImagePath() const
     if (color==PieceColor::black) return ":/images/KnightB.png";
     else return ":/images/KnightW.png";
 }
+
+
+const std::vector<Move>& Knight::getLegalMoves()
+{
+    generateLegalMovesAndKingDangers();
+    return legalMoves;
+}
+
+const std::vector<Location>& Knight::getKingDangerSquarePositions()
+{
+    generateLegalMovesAndKingDangers();
+    return kingDangerSquareLocations;
+}
+
+void Knight::generateLegalMovesAndKingDangers()
+{
+    legalMoves.clear();
+    kingDangerSquareLocations.clear();
+
+    auto x = location.x - 2;
+    auto y = location.y + 1;
+    if (x>=0 && y<8)
+    {
+        tryAddLegalMove(x, y);
+        tryAddKingDangerSquarePos(x, y);
+    }
+
+    x = location.x - 2;
+    y = location.y - 1;
+    if (x>=0 && y>=0)
+    {
+        tryAddLegalMove(x, y);
+        tryAddKingDangerSquarePos(x, y);
+    }
+
+    x = location.x - 1;
+    y = location.y - 2;
+    if (x>=0 && y>=0)
+    {
+        tryAddLegalMove(x, y);
+        tryAddKingDangerSquarePos(x, y);
+    }
+
+    x = location.x + 1;
+    y = location.y - 2;
+    if (x<8 && y>=0)
+    {
+        tryAddLegalMove(x, y);
+        tryAddKingDangerSquarePos(x, y);
+    }
+
+    x = location.x + 2;
+    y = location.y - 1;
+    if (x<8 && y>=0)
+    {
+        tryAddLegalMove(x, y);
+        tryAddKingDangerSquarePos(x, y);
+    }
+
+    x = location.x + 2;
+    y = location.y + 1;
+    if (x<8 && y<8)
+    {
+        tryAddLegalMove(x, y);
+        tryAddKingDangerSquarePos(x, y);
+    }
+
+    x = location.x - 1;
+    y = location.y + 2;
+    if (x>=0 && y<8)
+    {
+        tryAddLegalMove(x, y);
+        tryAddKingDangerSquarePos(x, y);
+    }
+
+    x = location.x + 1;
+    y = location.y + 2;
+    if (x<8 && y<8)
+    {
+        tryAddLegalMove(x, y);
+        tryAddKingDangerSquarePos(x, y);
+    }
+}
--- ChessPP/knight.h
+++ ChessPP/knight.h
@@ -8,6 +8,12 @@ class Knight final: public Piece
 public:
     Knight(unsigned short x, unsigned short y, PieceColor color, std::shared_ptr<Board> board): Piece(x, y, color, board){};
     [[nodiscard]] const std::string getImagePath() const;
+
+    [[nodiscard]] const std::vector<Move>& getLegalMoves();
+    [[nodiscard]] const std::vector<Location>& getKingDangerSquarePositions();
+
+private:
+    void generateLegalMovesAndKingDangers();
 };
 
 #endif // KNIGHT_H
